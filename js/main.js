f629d9d4f9557a6b3a3dd3234c217f0e5f5227dd
Deivintir <d4vidyeva@gmail.com>
2024-01-15 13:02:03 +0100

    add main.js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+//Template literals en strings.
+//ECMAScript2015 incorpora a JavaScript una funcionalidd que en muchos otros lenguajes conocemos como interpolación, implementa lectura de expresiones dentro de un string
+//evita concantenar en exceso fragmentos de caracteres de este tipo de dato.
+//Ejemplo:
